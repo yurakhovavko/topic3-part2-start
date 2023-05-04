1de5801987577f6bae4d2f1f5a49ec26c5d174b0
krosbite <killer1233215@mail.ru>
2023-05-04 22:52:37 +0300

    header

--- app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
+++ app/src/main/java/com/topic3/android/reddit/screens/HomeScreen.kt
@@ -1,8 +1,9 @@
 package com.topic3.android.reddit.screens
 
 import androidx.compose.runtime.Composable
+import com.topic3.android.reddit.viewmodel.MainViewModel
 
 @Composable
-fun HomeScreen() {
+fun HomeScreen(viewModel: MainViewModel) {
 
 }
