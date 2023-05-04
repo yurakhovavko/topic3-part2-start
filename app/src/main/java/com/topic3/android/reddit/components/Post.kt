a609c0b61086fecaa059b9b508430fc6c2fd4132
krosbite <killer1233215@mail.ru>
2023-05-04 22:58:22 +0300

    button/voit_action

--- app/src/main/java/com/topic3/android/reddit/components/Post.kt
+++ app/src/main/java/com/topic3/android/reddit/components/Post.kt
@@ -57,7 +57,35 @@ fun Post(post: PostModel, content: @Composable () -> Unit = {}) {
 
 @Composable
 fun Header(post: PostModel) {
-  //TODO add your code here
+  Row(modifier = Modifier.padding(start = 16.dp)){
+    Image(
+      ImageBitmap.imageResource(id = R.drawable.subreddit_placeholder),
+      contentDescription = stringResource(id = R.string.subreddits),
+      Modifier
+        .size(40.dp)
+        .clip(CircleShape)
+    )
+    Spacer(modifier = Modifier.width(8.dp))
+    Column (modifier = Modifier.weight(1f)){
+      Text(
+        text = stringResource(
+          R.string.subreddit_header,
+          post.subreddit
+        ),
+        fontWeight = FontWeight.Medium,
+        color = MaterialTheme.colors.primaryVariant
+      )
+      Text(
+        text = stringResource(
+          R.string.post_header,
+          post.username, post.postedTime
+        ),
+        color = Color.Gray
+      )
+    }
+    MoreActionsMenu()
+  }
+  Title(text = post.title)
 }
 
 @Composable
@@ -178,12 +206,34 @@ fun VotingAction(
   onUpVoteAction: () -> Unit,
   onDownVoteAction: () -> Unit
 ) {
-  //TODO add your code here
+  Row(verticalAlignment = Alignment.CenterVertically){
+    ArrowButton(
+      onUpVoteAction,
+      R.drawable.ic_baseline_arrow_upward_24
+    )
+    Text(
+      text = text,
+      color = Color.Gray,
+      fontWeight = FontWeight.Medium,
+      fontSize = 12.sp
+    )
+    ArrowButton(onDownVoteAction, R.drawable.ic_baseline_arrow_downward_24)
+  }
 }
 
 @Composable
 fun ArrowButton(onClickAction: () -> Unit, arrowResourceId: Int) {
-  //TODO add your code here
+  IconButton(
+    onClick = onClickAction,
+    modifier = Modifier.size(30.dp)
+  ) {
+    Icon(
+      imageVector = ImageVector.vectorResource(arrowResourceId),
+      contentDescription = stringResource(id = R.string.upvote),
+      modifier = Modifier.size(20.dp),
+      tint = Color.Gray
+    )
+  }
 }
 
 @Composable
